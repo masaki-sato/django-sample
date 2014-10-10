f5f7714f11e9a6a20c5d6539f64a030ae12df2a5
checkpoint <checkpoint777@gmail.com>
2014-10-10 13:28:45 +0900

    change settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -63,25 +63,25 @@ TEMPLATE_DIRS = (
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
+}
+
+
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'blog_db',
+#        'USER': 'blog',
+#        'PASSWORD': 'changeme',
+#        'HOST': 'localhost',
+#       'PORT': '3306',
+#        'OPTIONS': {"init_command": "SET storage_engine=INNODB"},
+#   }
 #}
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_db',
-        'USER': 'blog',
-        'PASSWORD': 'changeme',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {"init_command": "SET storage_engine=INNODB"},
-    }
-}
 
 
 # Internationalization
